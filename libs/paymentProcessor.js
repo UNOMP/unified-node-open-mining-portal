eb492175210b792b660f70344ec91749353f1d7b
Glen <daygle@users.noreply.github.com>
2017-12-09 22:50:19 +1100

    Update paymentProcessor.js

--- libs/paymentProcessor.js
+++ libs/paymentProcessor.js
@@ -3,8 +3,8 @@ var fs = require('fs');
 var redis = require('redis');
 var async = require('async');
 
-var Stratum = require('merged-pooler');
-var util = require('merged-pooler/lib/util.js');
+var Stratum = require('stratum-pool');
+var util = require('stratum-pool/lib/util.js');
 
 
 module.exports = function(logger){
