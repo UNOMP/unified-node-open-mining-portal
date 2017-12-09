9a9ffa1c923eeb55aaa9920031b3c012cc1b6b69
Glen <daygle@users.noreply.github.com>
2017-12-09 22:31:23 +1100

    Update init.js

--- init.js
+++ init.js
@@ -14,7 +14,7 @@ var PaymentProcessor = require('./libs/paymentProcessor.js');
 var Website = require('./libs/website.js');
 var ProfitSwitch = require('./libs/profitSwitch.js');
 
-var algos = require('merged-pooler/lib/algoProperties.js');
+var algos = require('stratum-pool/lib/algoProperties.js');
 
 JSON.minify = JSON.minify || require("node-json-minify");
 
